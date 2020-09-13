1ec081dc7d392ff121b4e1e358b4c4f851de1abc
Ryan Durkoske <rdurkoske@gmail.com>
2020-09-13 05:18:27 -0400

    completely recreated project environement. previous file structure was disorganized

---
+++ Speedrun/Screen.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SDL.h>
+
+class Screen {
+public:
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+
+	SDL_Rect* dimensions;
+
+	Screen(const char* title, int w, int h);
+
+	~Screen();
+
+	inline bool isClosed() const { return _closed; }
+
+	void pollEvents();
+
+private:
+	bool init(const char* title, int w, int h);
+	bool _closed;
+
+	
+};
