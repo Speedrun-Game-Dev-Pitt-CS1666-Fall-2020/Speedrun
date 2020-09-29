107b4027fedd732c8ac6c9b79a5e81b04f4a07f8
Connor Kalina <cjk94@pitt.edu>
2020-09-29 17:58:22 -0400

    fall off screen

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,181 +1,1 @@
-#include "Player.h"
-#include <cmath>
-
-Player::Player(float x, float y, int w, int h, SDL_Texture *t) : x_pos{x}, y_pos{y}, width{w}, height{h}, player_texture{t}
-{
-    x_vel = 0;
-    y_vel = 0;
-    x_accel = 0;
-    //gravity
-    y_accel = 0;
-    cantJump = true;
-    friction = .3;
-}
-
-void Player::updatePosition()
-{
-	//move horizontally, constrained by screen width
-    x_pos += x_vel;
-	if (x_pos  < 0 || (x_pos  + 100 > 1280))//100 = tile size, 1280 = screen width
-			x_pos  -= x_vel;
-	//move vertically, contrained by depth of world
-    y_pos += y_vel;
-	if (y_pos < 0 || (y_pos + 100 > 3000)) //100 = tile size, 3000 = world depth 
-			y_pos -= y_vel;
-}
-
-//probably will pass in array of rects that will s force
-void Player::applyForces()
-{
-
-    //for colliding with bottom of screen
-    //apply gravity. by default starts at gravity of 1
-
-    x_vel += x_accel;
-    
-    if (x_vel > 0)
-    {
-        if (x_vel - friction < 0)
-        {
-            x_vel = 0;
-        }
-        else
-        {
-            x_vel -= friction;
-        }
-        
-    }
-    else if (x_vel < 0) 
-    {
-        if (x_vel + friction > 0)
-        {
-            x_vel = 0;
-        }
-        else
-        {
-            x_vel += friction;
-        }
-    }
-
-    if (x_vel > 4)
-        x_vel = 4;
-
-    if (x_vel < -4)
-        x_vel = -4;
-
-    y_vel += y_accel;
-
-
-}
-
-void Player::detectCollisions(std::vector <SDL_Rect> r)
-{
-
-    //cant jump unless detech collision with some floor
-    cantJump = true;
-
-    //go thru all possible collision things and checkCollision/apply force
-    //for colliding with bottom of screen
-    if (y_pos >= 2970 - 100)
-    {
-        y_pos = 2970-100;
-        //y_vel = 0;
-        //y_accel = 0;
-        if (y_vel > 0)
-        {
-            y_vel = 0;
-        }
-        cantJump = false;
-    }
-
-    for (auto block: r)
-    {
-        if (isColliding(&block))
-            handleCollision(&block);
-    }
-    // if (isColliding(r))
-    //     handleCollision(r);
-    // if (isColliding(r2))
-    //     handleCollision(r2);
-}
-
-bool Player::isColliding(SDL_Rect *r)
-{
-    // Check vertical overlap
-    if (y_pos + height <= r->y)
-        return false;
-    if (y_pos >= r->y + r->h)
-        return false;
-
-    // Check horizontal overlap
-    if (x_pos >= r->x + r->w)
-        return false;
-    if (x_pos + width <= r->x)
-        return false;
-
-    // Must overlap in both
-    return true;
-}
-
-//assuming detect collisions will never change player velocity variables
-//spring traps + friction and stuff change accel variables
-void Player::handleCollision(SDL_Rect *r)
-{
-    //Check player velocity direction for
-
-    //get the x and y constraints for box based off of velocity direction
-    float requiredX = 0;
-    float requiredY = 0;
-
-    if (x_vel < 0)
-    {
-        //hitting collision object from right
-        //need x pos of player to be box + boxwidth
-        requiredX = r->x + r->w;
-    }
-    else
-    {
-        //hitting collision object from left
-        //need x pos of player to be box - playerwidth
-        requiredX = r->x - width;
-    }
-
-    if (y_vel < 0)
-    {
-        //hitting collision object from bot
-        //need y pos of player to be box+boxheight
-        requiredY = r->y + r->h;
-    }
-    else
-    {
-        //hitting collision object from top
-        //need y pos of player to be box - playerheight
-        requiredY = r->y - height;
-        //moved on top of object
-        cantJump = false;
-    }
-
-    //what is closer? complementary X to x_pos or complementary y to y_pos?
-    if (fabs(y_pos - requiredY) < fabs(x_pos - requiredX))
-    {
-        //x_pos = complementaryX;
-        y_pos = requiredY;
-        y_vel = 0;
-
-        if (r->w == 100)
-        {
-            y_vel += -30;
-            cantJump = true;
-        }
-    }
-    else
-    {
-        //use requiredX bc compY closer
-        x_pos = requiredX;
-        //y_pos = complementaryY;
-        x_vel = 0;
-        x_accel = 0;
-    }
-
-    //if moving towards collisions in either x or y, set velocity in that direction to 0
-}
+#include "Player.h"#include <cmath>Player::Player(float x, float y, int w, int h, SDL_Texture *t) : x_pos{x}, y_pos{y}, width{w}, height{h}, player_texture{t}{    x_vel = 0;    y_vel = 0;    x_accel = 0;    //gravity    y_accel = 1;    cantJump = true;    friction = .3;}void Player::updatePosition(){	//move horizontally, constrained by screen width    x_pos += x_vel;	if (x_pos  < 0 || (x_pos  + 20 > 1280))//100 = tile size, 1280 = screen width			x_pos  -= x_vel;	//move vertically, contrained by depth of world    y_pos += y_vel;	if (y_pos < 0 || (y_pos + 20 > 720)) //100 = tile size, 3000 = world depth 			y_pos -= y_vel;}//probably will pass in array of rects that will apply forcevoid Player::applyForces(){    //for colliding with bottom of screen    //apply gravity. by default starts at gravity of 1    x_vel += x_accel;        if (x_vel > 0)    {        if (x_vel - friction < 0)        {            x_vel = 0;        }        else        {            x_vel -= friction;        }            }    else if (x_vel < 0)     {        if (x_vel + friction > 0)        {            x_vel = 0;        }        else        {            x_vel += friction;        }    }    if (x_vel > 4)        x_vel = 4;    if (x_vel < -4)        x_vel = -4;    y_vel += y_accel;}void Player::detectCollisions(std::vector <SDL_Rect> r){    //cant jump unless detech collision with some floor    cantJump = true;    //go thru all possible collision things and checkCollision/apply force    //for colliding with bottom of screen    if (y_pos >= 3000 - height)    {        y_pos = 3000 - height;        //y_vel = 0;        //y_accel = 0;        if (y_vel > 0)        {            y_vel = 0;        }        cantJump = false;    }    for (auto block: r)    {        if (isColliding(&block))            handleCollision(&block);    }    // if (isColliding(r))    //     handleCollision(r);    // if (isColliding(r2))    //     handleCollision(r2);}bool Player::isColliding(SDL_Rect *r){    // Check vertical overlap    if (y_pos + height <= r->y)        return false;    if (y_pos >= r->y + r->h)        return false;    // Check horizontal overlap    if (x_pos >= r->x + r->w)        return false;    if (x_pos + width <= r->x)        return false;    // Must overlap in both    return true;}//assuming detect collisions will never change player velocity variables//spring traps + friction and stuff change accel variablesvoid Player::handleCollision(SDL_Rect *r){    //Check player velocity direction for    //get the x and y constraints for box based off of velocity direction    float requiredX = 0;    float requiredY = 0;    if (x_vel < 0)    {        //hitting collision object from right        //need x pos of player to be box + boxwidth        requiredX = r->x + r->w;    }    else    {        //hitting collision object from left        //need x pos of player to be box - playerwidth        requiredX = r->x - width;    }    if (y_vel < 0)    {        //hitting collision object from bot        //need y pos of player to be box+boxheight        requiredY = r->y + r->h;    }    else    {        //hitting collision object from top        //need y pos of player to be box - playerheight        requiredY = r->y - height;        //moved on top of object        cantJump = false;    }    //what is closer? complementary X to x_pos or complementary y to y_pos?    if (fabs(y_pos - requiredY) < fabs(x_pos - requiredX))    {        //x_pos = complementaryX;        y_pos = requiredY;        y_vel = 0;        if (r->w == 100)        {            y_vel += -30;            cantJump = true;        }    }    else    {        //use requiredX bc compY closer        x_pos = requiredX;        //y_pos = complementaryY;        x_vel = 0;        x_accel = 0;    }    //if moving towards collisions in either x or y, set velocity in that direction to 0}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
 #include <SDL.h>
 #include <SDL_image.h>
 #include "XorShifter.h"
@@ -14,43 +15,48 @@
 constexpr int SCREEN_WIDTH = 1280;
 constexpr int SCREEN_HEIGHT = 720;
 
+//for move player tutorial, may move to player object later
+constexpr int BOX_SIZE = 20;
 constexpr int WORLD_DEPTH = 3000;
-constexpr int TILE_SIZE = 100;
+
 // Globals
 Screen *screen = nullptr;
 Uint32 before;
 Uint32 then;
 Uint32 delta;
 Uint32 now;
+std::vector <SDL_Rect> blocks;
+std::vector <SDL_Rect> decorative_blocks;
 
 // Function declarations
 bool init();
 void close();
 Image *loadImage(const char *, int, int);
-SDL_Texture* loadTexture(std::string fname);
 void runCredits();
 void runGame();
 void runMenu();
-void generateTerrain();
-//globals
-SDL_Texture* gBackground;
-SDL_Texture* dude;
+Player* generateTerrain();
+SDL_Texture *loadTexture(std::string);
 
 Image *loadImage(const char *src, int w, int h)
 {
 	return new Image(screen, src, 0, 0, w, h);
 }
-SDL_Texture* loadTexture(std::string fname) {
-	SDL_Texture* newText = nullptr;
 
-	SDL_Surface* startSurf = IMG_Load(fname.c_str());
-	if (startSurf == nullptr) {
+SDL_Texture *loadTexture(std::string fname)
+{
+	SDL_Texture *newText = nullptr;
+
+	SDL_Surface *startSurf = IMG_Load(fname.c_str());
+	if (startSurf == nullptr)
+	{
 		std::cout << "Unable to load image " << fname << "! SDL Error: " << SDL_GetError() << std::endl;
 		return nullptr;
 	}
 
 	newText = SDL_CreateTextureFromSurface(screen->renderer, startSurf);
-	if (newText == nullptr) {
+	if (newText == nullptr)
+	{
 		std::cout << "Unable to create texture from " << fname << "! SDL Error: " << SDL_GetError() << std::endl;
 	}
 
@@ -84,6 +90,77 @@ void close()
 	delete screen;
 	// Quit SDL subsystems
 	SDL_Quit();
+}
+
+//create blocks, add them to the blocks vector, and return a player at a valid spawn point
+Player* generateTerrain()
+{
+	blocks.clear();
+	decorative_blocks.clear();
+	
+	srand (time(NULL));
+	int rand (void);
+	
+	XorShifter *rng = new XorShifter(412001000);
+	SimplexNoise *simp = new SimplexNoise(rand() % 1000000);
+	simp->freq = 0.05f;
+	simp->octaves = 2;
+	simp->updateFractalBounds();
+	
+	int cave_nums[SCREEN_HEIGHT / BOX_SIZE];
+	
+	//SDL_Texture* block_texture = loadTexture("../res/block.png");
+	//SDL_Texture* background_texture = loadTexture("../res/background_block.png");
+	
+	//generate values from a "line" of noise, one value for each row of blocks
+	for (int test = -18; test < 18; test++)
+	{
+		float val_f = simp->getSingle(1, test) * 100;
+		
+		int val_i = (int)val_f;
+		
+		cave_nums[test + 18] = val_i;
+	}
+	
+	bool player_created = false;
+	Player *user;// = new Player(10, 0, 20, 20, loadTexture("../res/Guy.png"));
+	
+	//for each block on the screen
+	for (int y = 0; y < SCREEN_HEIGHT; y = y + BOX_SIZE)
+	{
+		
+		for (int x = 0; x < SCREEN_WIDTH; x = x + BOX_SIZE)
+		{
+			//x position of block / screen width
+			int ratio = (float)x / (float)SCREEN_WIDTH * 100;
+			
+			
+			//if the relative position of the block on the screen is more than 10 below or above 
+			//the given noise value for this row, render it as part of the walls
+			if ((ratio < cave_nums[y / BOX_SIZE] - 10) || (ratio > cave_nums[y / BOX_SIZE] + 10))
+			{	
+				SDL_Rect block = {x, y, BOX_SIZE, BOX_SIZE};
+				blocks.push_back(block);
+			}
+			//else, render it as part of the cave
+			else
+			{
+				//SDL_Rect block = {x, y, BOX_WIDTH, BOX_HEIGHT};
+				
+				//spawn the player in the first empty block
+				if (!player_created)
+				{
+					user = new Player(x, y, 20, 20, loadTexture("../res/Guy.png"));
+					player_created = true;
+				}
+				
+				//decorative_blocks.push_back(block);
+			}
+		}
+	}
+	
+	return user;
+	
 }
 
 void runCredits()
@@ -158,29 +235,19 @@ void runCredits()
 
 void runGame()
 {
-	//my stuff
-	//generateTerrain();
-	gBackground = loadTexture("../res/background.png");
-	int scroll_offset = 0;
-	int rem = 0;
-	SDL_Rect moveBox = {SCREEN_WIDTH/2 - TILE_SIZE/2, SCREEN_HEIGHT/2 - TILE_SIZE/2, TILE_SIZE, TILE_SIZE};	
-
-	
-	int tthird = (SCREEN_HEIGHT / 3) - TILE_SIZE/2;
-	int bthird = (2 * SCREEN_HEIGHT/ 3) - TILE_SIZE/2;
-	
-	SDL_Rect playercam = {SCREEN_WIDTH/2 - TILE_SIZE/2, SCREEN_HEIGHT/2 - TILE_SIZE/2, TILE_SIZE, TILE_SIZE};
-	SDL_Rect bgRect = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
-	
 	// Create player object with x, y, w, h, texture
-	Player *user = new Player(10, 0, 100, 100, loadTexture("../res/Guy.png"));
+	//Player *user = new Player(10, 0, 20, 20, loadTexture("../res/Guy.png"));
+	
+	//create the player and generate the terrain
+	Player *user = generateTerrain();
+	//mine
 
+	
 	//Define the blocks
-	SDL_Rect block = {900, 2900, 200, 20};
+	/*SDL_Rect block = {SCREEN_WIDTH/2, SCREEN_HEIGHT-20, 200, 20};
 	SDL_Rect anotherBlock = {SCREEN_WIDTH/2 - 190, SCREEN_HEIGHT-120, 120, 20};
 	SDL_Rect spring = {SCREEN_WIDTH/2 - 300, SCREEN_HEIGHT-180, 100, 20};
-
-	std::vector <SDL_Rect> blocks = {block, anotherBlock, spring};
+	blocks = {block, anotherBlock, spring};*/
 
 	SDL_Event e;
 	bool gameon = true;
@@ -192,89 +259,71 @@ void runGame()
 		//get intended motion based off input
 		while (SDL_PollEvent(&e))
 		{
-			if (e.type == SDL_QUIT)
+			const Uint8 *keystate = SDL_GetKeyboardState(nullptr);
+			if (e.type == SDL_QUIT || keystate[SDL_SCANCODE_ESCAPE])
 			{
 				gameon = false;
 			}
-		}
-		const Uint8 *keystate = SDL_GetKeyboardState(nullptr);
-		if(keystate[SDL_SCANCODE_W]){
+			else
+			{
 
-				if (!user->cantJump)
-				{
-					user->y_vel += -15;
-					user->cantJump = true;
+				if(keystate[SDL_SCANCODE_W]){
+
+					if (!user->cantJump)
+					{
+						user->y_vel += -15;
+						user->cantJump = true;
+					}
+
 				}
-
-			}
-		if(keystate[SDL_SCANCODE_A]){
-				user->x_accel = -0.5;
+				if(keystate[SDL_SCANCODE_A]){
+					user->x_accel = -0.5;
 					// if(user->x_vel > -4){
 					// 	user->x_vel += -2;
 					// }
-			}
-		if(keystate[SDL_SCANCODE_D]){
-				user->x_accel = 0.5;
+				}
+				if(keystate[SDL_SCANCODE_D]){
+					user->x_accel = 0.5;
 					// if(user->x_vel < 4){
 					// 	user->x_vel += 2;
 					// }
-			}
-		if(keystate[SDL_SCANCODE_S]){
-					user->y_accel = 0.5;
-			}
+				}
+				if(keystate[SDL_SCANCODE_S]){
+					
+				}
 
-		if (!keystate[SDL_SCANCODE_A] && !keystate[SDL_SCANCODE_D])
-			{
-				user->x_accel = 0;
+				if (!keystate[SDL_SCANCODE_A] && !keystate[SDL_SCANCODE_D])
+				{
+					user->x_accel = 0;
+				}
 			}
-	
+		}
+
 		// Move box
 		user->updatePosition();
-		//new
-		if (user->y_pos < (scroll_offset + tthird))
-			scroll_offset = user->y_pos - tthird;
-		else if (user->y_pos > (scroll_offset + bthird))
-			scroll_offset = user->y_pos - bthird;
 
-		if (scroll_offset < 0)
-			scroll_offset = 0;
-
-		if (scroll_offset + SCREEN_HEIGHT > WORLD_DEPTH)
-			scroll_offset = WORLD_DEPTH - SCREEN_HEIGHT;
 		//check constraints and resolve conflicts
 		//apply forces based off gravity and collisions
-		user->detectCollisions(blocks);
+		
 		
 		// Clear black
 		SDL_SetRenderDrawColor(screen->renderer, 0x00, 0x00, 0x00, 0xFF);
 		SDL_RenderClear(screen->renderer);
-		//new
-		rem = scroll_offset % SCREEN_HEIGHT;
-		bgRect.y = -rem;
-		SDL_RenderCopy(screen->renderer, gBackground, nullptr, &bgRect);
-		bgRect.y += SCREEN_HEIGHT;
-		SDL_RenderCopy(screen->renderer, gBackground, nullptr, &bgRect);
+
 		// Draw boxes
 		SDL_SetRenderDrawColor(screen->renderer, 0xFF, 0x00, 0x00, 0xFF);
 		
+		for (auto b: blocks)
+		{
+			b.y = b.y - (user->y_vel);
+			b.x = b.x - (user->x_vel);
+			SDL_RenderFillRect(screen->renderer, &b);
+		}
+		user->detectCollisions(blocks);
 
-		//new
-		playercam.y = user->y_pos - scroll_offset;
-		playercam.x = user->x_pos;
 		// Player box
 		SDL_Rect player_rect = {user->x_pos, user->y_pos, user->width, user->height};
-		SDL_RenderCopy(screen->renderer, user->player_texture, NULL, &playercam);
-		
-		for (auto b: blocks)
-		{//on screen  if b.y = bgRect.y to bgRect.y + 720
-	//position on screen = b.y - bgRect.y
-			if(b.y >= bgRect.y && b.y <= bgRect.y + 720)
-			{
-				SDL_Rect tempBlock = {b.x, b.y-bgRect.y,b.w, b.h};
-				SDL_RenderFillRect(screen->renderer, &tempBlock);
-			}
-		}
-		
+		SDL_RenderCopy(screen->renderer, user->player_texture, NULL, &player_rect);
 		SDL_RenderPresent(screen->renderer);
 	}
 }
@@ -333,41 +382,7 @@ void runMenu()
 		}
 	}
 }
-void generateTerrain()
-{
-	SimplexNoise *simp = new SimplexNoise(420);
-	simp->freq = 0.07f;
-	simp->octaves = 2;
-	simp->updateFractalBounds();
-	int cave_nums[SCREEN_HEIGHT / TILE_SIZE];
-	//load texture
-	SDL_Texture* block_texture = loadTexture("../res/block.png");
-	SDL_Texture* background_texture = loadTexture("../res/background_block.png");
-	
-	for (int test = -18; test < 18; test++)
-	{
-		float val_f = simp->getSingle(1,test)*100;
-		int val_i = (int)val_f;
-		cave_nums[test+18] = val_i;
-	}
-	for (int y = 0; y < SCREEN_HEIGHT; y = y + TILE_SIZE)
-	{
-		for(int x = 0; x < SCREEN_WIDTH; x = x + TILE_SIZE)
-		{
-			int ratio = (float)x / (float)SCREEN_WIDTH * 100;
-			if((ratio <cave_nums[y / TILE_SIZE] - 10) || (ratio > cave_nums[y / TILE_SIZE] + 10))
-			{
-				SDL_Rect block = {x,y,TILE_SIZE,TILE_SIZE};
-				SDL_RenderCopy(screen->renderer,block_texture,NULL,&block);
-			}
-			else
-			{
-				SDL_Rect block = {x,y,TILE_SIZE,TILE_SIZE};
-				SDL_RenderCopy(screen->renderer,background_texture,NULL,&block);
-			}
-		}
-	}
-}
+
 int main(int argc, char **argv)
 {
 
